7b56e4a3ba1a25f11b0d95fcbde5c6bc65dd6b41
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-16 22:35:37 +0200

    introduced basic controls: text input radio buttons

--- UI.R
+++ UI.R
@@ -5,19 +5,16 @@ library(shinythemes)
 shinyUI(navbarPage(theme = "bootstrap.css" ,id = "bar" ,
                    #HOME PANEL     
                    tabPanel("home"
-                        
                             ),
-                   
                    tabPanel(HTML("TweetIT</a></li><li><a style='color:#3399FF' 
                                  href=\"https://it.linkedin.com/in/cirilloandrea\">by Andrea Cirillo</a>"),
-                            
-                            h3("here we are")
-                           
-                                     ),
-                   
-                   #SCRIPTS
-                   
-                  
+                            h3(""),
+                            textInput("selected_keyword",h3("input keyword to look for on Twitter")),
+                                      radioButtons("selected_language",
+                                                   "select language",
+                                                   choices = c("italian","english"),
+                                                   inline = TRUE)
+                                      ),
                    windowTitle = "TweetIT"
-                   
-                              ))
+                                      )
+                              )
