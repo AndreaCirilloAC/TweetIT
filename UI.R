dc75e315cf28a891e1e2989ffc3abe0985fa53f4
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-21 08:47:19 +0200

    introduced FluidRow with plotoutput control with cloud plot

--- UI.R
+++ UI.R
@@ -3,7 +3,7 @@ library(shinyBS)
 library(shinythemes)
 
 shinyUI(navbarPage(theme = "bootstrap.css" ,id = "bar" ,
-                   #HOME PANEL     
+                   #HOME PANEL with Keywork selection and language setting  
                    tabPanel("home"
                             ),
                    tabPanel(HTML("TweetIT</a></li><li><a style='color:#3399FF' 
@@ -13,22 +13,25 @@ shinyUI(navbarPage(theme = "bootstrap.css" ,id = "bar" ,
                               column(4,
                             div(style = "text-align: center;",
                             textInput("selected_keyword",h3("input keyword to look for on Twitter")),
-                                      radioButtons("selected_language",
+                            radioButtons("selected_language",
                                                    h3("select language"),
                                                    choices = c("italian","english"),
                                                    inline = FALSE)
+                            
                             )
                             ),
-                            
                             column(4)),
                             fluidRow(
                             div(style = "text-align: center;",
                             bsButton("sentiment","sentimentIT",
                                      style = "default",size = "large")
                             )
+                            ),
+                            fluidRow(
+                              div(style = "text-align: center;",
+                                  h3( "wordcloud for the given keyword"),
+                                  plotOutput("cloud"))
                             )
-                            
-                            
                                       ),
                    windowTitle = "TweetIT"
                                       )
