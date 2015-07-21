bf2e234f2fb209b8fcfb14d8d111cf774007d0e6
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-21 12:28:49 +0200

    script for plotoutput on UI

--- server.R
+++ server.R
@@ -1,8 +1,11 @@
 shinyServer(function(input, output,session) {
-  output$cloud <- renderPlot(
-    keyword = input$selected_keyword,
-    source("main.R",verbose =  TRUE),
-    return(cloud)
+  #keyword <- reactive(input$selected_keyword)
+  output$cloud <- renderPlot({
+    keyword = input$selected_keyword
     
-  )
+    source("main.R",verbose =  TRUE)
+    
+    return(cloud)
+
+    })
 })
