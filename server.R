d16fd686e52247d3d1f4c91edf3cdcc4dc308abb
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-21 08:47:46 +0200

    first script on server, cloud plot

--- server.R
+++ server.R
@@ -1,1 +1,8 @@
-shinyServer(function(input, output,session) {})
+shinyServer(function(input, output,session) {
+  output$cloud <- renderPlot(
+    keyword = input$selected_keyword,
+    source("main.R",verbose =  TRUE),
+    return(cloud)
+    
+  )
+})
