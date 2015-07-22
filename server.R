769e68683014098b8130fe47fb22bc89a745c9dc
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-22 07:55:52 +0200

    insert conditional statement to renderplot instruction
    Plot rendered only when keyword is written and sentiment is pushed

--- server.R
+++ server.R
@@ -1,11 +1,14 @@
 shinyServer(function(input, output,session) {
-  #keyword <- reactive(input$selected_keyword)
+  
   output$cloud <- renderPlot({
+    if (input$sentiment & input$selected_keyword != "") 
+    {
     keyword = input$selected_keyword
     
     source("main.R",verbose =  TRUE)
     
     return(cloud)
-
+    }else
+      return()
     })
 })
